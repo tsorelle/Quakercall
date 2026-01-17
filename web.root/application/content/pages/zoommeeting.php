beb35a48eee95c92d15e9373d888ebafe81d4923
tsorelle <terry.sorelle@outlook.com>
2026-01-17 10:05:34 -0600

    added static page for zoom access in case of problems

---
+++ web.root/application/content/pages/zoommeeting.php
@@ -0,0 +1,10 @@
+<h1>Welcome to the Quaker Call to Action National Meeting</h1>
+<h3>Use this information to join the Zoom meeting.</h3>
+
+<p>
+    <a href="https://us06web.zoom.us/j/88394251008?pwd=oKL3mH6zoBzRK7cxe42BKGNRopPxeK.1">Click here to join...</a>
+</p>
+<p>
+    Meeting ID: 883 9425 1008<br>
+    Passcode: quakers
+</p>
