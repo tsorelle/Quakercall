dcbd0e5e35dbe5e7909442406718daf1586a05f5
tsorelle <terry.sorelle@outlook.com>
2026-01-21 19:26:21 -0600

    update page header

--- web.root/application/content/page.php
+++ web.root/application/content/page.php
@@ -29,6 +29,7 @@
 ?>
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="/application/assets/img/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
