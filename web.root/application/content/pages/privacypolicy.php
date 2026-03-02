c5fc49bf5e892b47875e5dc581d398cd3202aa44
tsorelle <terry.sorelle@outlook.com>
2026-03-02 10:31:19 -0600

    add privacy policy page

---
+++ web.root/application/content/pages/privacypolicy.php
@@ -0,0 +1,24 @@
+<h1>Quaker Call to Action<br>Privacy Policy</h1>
+<p><strong><em>Quaker Call to Action</em></strong> (quakercall.net and quakercall.org)
+    maintains lists of persons and organizations
+    who have registered their endorsement of the call, registered to attend an on-line event,
+    or simply requested to be on our email mailing list. In addition to the persons name
+    we may optionally include email addresses, phone numbers and postal addresses or
+    location information such as city, state, and country. We only store that information
+    which the person as voluntarily provided to us for communications purposes.</p>
+
+<p>We do not add to these lists unless the person has specifically requested to receive
+    communications from us. And any recipient can "unsubscribe" or "opt-out" at any time.
+    You can do so by clicking a link in an email you receive, or making a request directly
+    using our contact form.
+</p>
+
+<p>We do not share this data with any other organizations or individuals.</p>
+<p>We do not publish personal information such as email addresses and phone numbers
+in any location or our sites where the information would be visible to anonymous
+users or visitors.</p>
+
+<p>If you have any concerns regarding privacy, or wish us to discontinue any communication
+that you receive, use our contact form to send use a request or message:
+<a href="https://quakercall.org/contact">Contact Quaker Call</a>
+</p>
