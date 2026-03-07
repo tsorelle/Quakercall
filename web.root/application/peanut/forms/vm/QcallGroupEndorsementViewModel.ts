8c259845a72ea9ec9b94bda522075fab37435772
tsorelle <terry.sorelle@outlook.com>
2026-03-07 09:48:30 -0600

    first deployment of new version

--- web.root/application/peanut/forms/vm/QcallGroupEndorsementViewModel.ts
+++ web.root/application/peanut/forms/vm/QcallGroupEndorsementViewModel.ts
@@ -6,13 +6,56 @@ namespace Peanut {
 
     export class QcallGroupEndorsementViewModel extends Peanut.ViewModelBase {
         // observables
+        tab = ko.observable('form');
+
+        form = {
+            fullname : ko.observable(''),
+            firstName : ko.observable(''),
+            lastName : ko.observable(''),
+            email : ko.observable(''),
+            phone : ko.observable(''),
+            city : ko.observable(''),
+            state : ko.observable(''),
+            postalCode : ko.observable(''),
+            country : ko.observable(''),
+            organization : ko.observable(''),
+            subscribed : ko.observable(false),
+            hearAbout: ko.observable(''),
+            comments: ko.observable(''),
+
+            emailError: ko.observable(false),
+            nameError: ko.observable(false),
+            religion : ko.observable('Quaker'),
+            religionOther : ko.observable('')
+        }
+
+        result = {
+            fullname 	   : ko.observable(''),
+            phone        : ko.observable(''),
+            location     : ko.observable(''),
+            email        : ko.observable(''),
+            organization : ko.observable(''),
+            submissionId : ko.observable(''),
+            startDate: ko.observable('')
+        }
+
+
 
         init(successFunction?: () => void) {
             let me = this;
-            Peanut.logger.write('Registration Init');
-            let fd = this.getPageVarialble('formdata');
+            Peanut.logger.write('Group endorsment Init');
+
             me.bindDefaultSection();
             successFunction();
         }
+
+        submitEndorsement = () => {
+            alert('submitting endorsement');
+        }
+
+        showPrivacyModal = () => {
+            this.showModal('privacy-modal')
+        }
+
     }
 }
